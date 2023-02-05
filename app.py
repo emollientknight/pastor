886371418e7070f3785270315ff47203d0a5c4af
Emollient Knight <emollient.knight@protonmail.com>
2023-02-04 17:42:00 -0800

    add env variables

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+import os
 from time import sleep
+from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 
 from sessions import Sessions
 
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
+load_dotenv(dotenv_path=dotenv_path)
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder=os.getenv("STATIC_FOLDER"), static_url_path="/")
 
 sessions = Sessions()
 
@@ -18,10 +22,8 @@ def ask():
     print(content_type)
     if (content_type == 'application/json'):
         json = request.json
-        # data = sessions.ask(json['token'], json['input'])
-        sleep(3)
-        # return jsonify({"data": data['choices'][0]['text']}), 200
-        return "test"
+        data = sessions.ask(json['token'], json['input'])
+        return jsonify({"data": data['choices'][0]['text']}), 200
     else:
         return 'Content-Type not supported!'
     
